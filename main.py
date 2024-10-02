a1605cde2cf65e0a2c4fc63f5c0137749387f4b1
marioguerra2002 <alu0101395036@ull.edu.es>
2024-10-02 11:40:24 +0100

    creación de matriz de adyacencia?

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 # Main que contiene la lógica de la aplicación y lectura de archivos.
-
+import sys
+from matriz import matriz
 def main():
-  fichero
-
+  datos = []
+  if len(sys.argv) != 2:
+    print ("Uso: python main.py <fichero>")
+    sys.exit(1)
+  input = sys.argv[1]
+  try:
+    i = 0
+    with open(input, "r") as f:
+      # leer linea por linea
+      fichero = f.readlines()
+      n = int(fichero[0])
+      prueba = float(fichero[1])
+      prueba2 = float(fichero[2])
+      prueba3 = float(fichero[3])
+      fichero.pop(0)
+      print ("Fichero: ", fichero)
+      print ("Numero de elementos: ", n)
+      print ("Prueba: ", prueba)
+      print ("Prueba2: ", prueba2)
+      print ("Prueba3: ", prueba3)
+      matriz(fichero, n)
+      # se le pasa de entrada ya las lineas del archivo y el tamaño de la matriz
+      
+  except FileNotFoundError:
+    print ("El fichero no existe")
+  except PermissionError:
+    print ("No tienes permisos para leer el fichero")
+  
+      # fichero contiene las lineas del archivo
 if __name__ == "__main__":
   main()
---
+++ matriz.py
@@ -0,0 +1,33 @@
+def matriz(input, size):
+  #se le pasa de entrada ya las lineas del archivo
+  #se guarda en una matriz
+  datos = [[-1.00] * size for _ in range(size)]
+  iterador_filas = 0
+  iterador_columnas = 0
+  iterador_datos = 0
+  # se crea una matriz de tamaño size x size con valores -1.00
+  
+  for iterador_filas in range (size):
+    print ("Valor de la columna: ", iterador_columnas)
+    for iterador_columnas in range(iterador_filas, size): # para que empiece en la siguiente columna
+      print ("Valor de la fila y columna: ", iterador_filas, iterador_columnas)
+      if iterador_columnas == iterador_filas:
+        #si son iguales es que es sí mismo
+        datos[iterador_filas][iterador_columnas] = 0.00
+      # evitar que el iterador de datos se salga del rango
+      if iterador_datos >= len(input):
+        break
+      datos[iterador_filas][iterador_columnas] = float(input[iterador_datos].strip()) # strip() elimina los espacios en blanco
+      iterador_columnas += 1
+      iterador_datos += 1
+      print ("Valor de la coumna dentro del bucle: ", iterador_columnas)
+    iterador_filas += 1
+    print ("Valor de la fila dentro del bucle: ", iterador_filas)
+    # iterador_columnas = iterador_filas # para que empiece en la siguiente fila
+  # for linea in input:
+  #   for i in range(size):
+  print(datos)
+      
+    
+  #   datos.append(float(linea.strip()))
+  #   # se guarda en una fila de la matriz todos los datos
